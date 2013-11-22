7bb59ce30afe9aebd4abca278ff1a82e72c9afe1
Cindy Ho <cindango@gmail.com>
2013-11-22 04:11:52 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sofa::Application.config.secret_token = '848eb16bc5792972204b50cba734ac583829baa55492d332218f3feaf4857b413d48a949e663d9987d1a058bdfa5dc2ee52006648210b5ced30c7f83bd536093'
